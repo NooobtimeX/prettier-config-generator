45e6bf4509cf9602c45911600a61161af353454b
wongsaphat puangsorn <nooobtimex@gmail.com>
2025-05-09 17:44:15 +0700

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-N3C2N4G7" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
